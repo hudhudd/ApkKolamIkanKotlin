78ab6f23b7503cff7124d9c7ad7f905798f7bf32
Yehuda <yehudaor987@gmail.com>
2021-10-18 08:24:32 +0700

    First Init

--- app/src/main/java/com/example/capskolwmanualnav/MainActivity.kt
+++ app/src/main/java/com/example/capskolwmanualnav/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.capskolwmanualnav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.capskolwmanualnav.fragment.AkunFragment
+import com.example.capskolwmanualnav.fragment.HomeFragment
+import com.example.capskolwmanualnav.fragment.RiwayatFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val fragmentHome: Fragment = HomeFragment()
+    val fragmentRiwayat: Fragment = RiwayatFragment()
+    val fragmentAkun: Fragment = AkunFragment()
+    val fm: FragmentManager = supportFragmentManager
+    var active: Fragment = fragmentHome
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fm.beginTransaction().add(R.id.canvas, fragmentHome).show(fragmentHome).commit()
     }
 }
